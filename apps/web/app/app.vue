424cce4000eae67069441f512fb70bd5005af249
すみそ <sumi@wattai.jp>
2026-09-18 20:54:21 +0900

    feat(v7): update apps/web/app/app.vue

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -5,26 +5,8 @@
  * ゲーム開始前はセットアップ画面、開始後は「1枚の卓面」(パン&ズーム可能)+ 上部固定HUD。
  * イベント解決・フェーズ終了清算・最終結果はモーダルで割り込む。
  */
-const { state, started } = useGame()
 </script>
 
 <template>
-  <div class="table-app">
-    <TableHud v-if="started" />
-    <ToastViolation />
-
-    <SetupPanel v-if="!started" />
-
-    <template v-else>
-      <TableStage />
-
-      <EventModal v-if="state.pendingEvent" />
-      <template v-else>
-        <PhaseEndModal v-if="state.step === 'phase_end'" />
-        <ResultModal v-else-if="state.step === 'finished'" />
-      </template>
-
-      <ActionLogScroll />
-    </template>
-  </div>
+  <V7Prototype />
 </template>
